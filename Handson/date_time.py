1cda4d086d87422f7f4ab472a950fa89fad3222a
Stephin <stephin.sebastian96@gmail.com>
2023-01-19 13:54:26 +0530

    -- 19 Jan 2023

---
+++ Handson/date_time.py
@@ -0,0 +1,42 @@
+# def stringchar(string):
+#     return f"Hello, {string} !"
+#
+# print("The name is : ", __name__)
+#
+# def add(num1, num2):
+#     return num1+num2+20
+# if __name__ == '__main__':
+#     print(stringchar("Stephin"))
+#     x = add(4,6)
+#     print(x)
+# import datetime
+# input_date = input("Date : ").title().strip()
+# print(input_date)
+# l = []
+# if '/' in input_date:
+#     date = input_date.split('/')
+#     l += date
+# elif ',' in input_date:
+#     date = input_date.replace(",","").split()
+#     l += date
+# print(l)
+
+# #Time module is used for the execution time of the code
+import time
+# initial = time.time()
+# k =0
+# while(k<45):
+#     print("Hello")
+#     k+=1
+# print("The execution time of while loop is :", time.time() - initial, "seconds")
+#
+# initial1 = time.time()
+# for i in range(45):
+#     print("Hello")
+# print("The execution time of for loop is : ",time.time() - initial1,"seconds")
+
+# To check the local-time
+# localtime = time.asctime(time.localtime(time.time()))
+# print(localtime)
+
+# Date and
